d1b62d41528b25a0a90ac9bb2e6e0db214eea8de
jessie <junchangchen@gmail.com>
2016-08-11 22:02:05 +0800

    chef

--- chef/cookbooks/djshop_ubuntu/attributes/default.rb
+++ chef/cookbooks/djshop_ubuntu/attributes/default.rb
@@ -1,4 +1,6 @@
 
+app_name = 'djshop'
+
 default['firewall']['allow_ssh'] = true
 default['djshop_ubuntu']['open_ports'] = 80
 
@@ -6,4 +8,6 @@ default['djshop_ubuntu']['user'] = 'web_admin'
 default['djshop_ubuntu']['group'] = 'web_admin'
 
 default['djshop_ubuntu']['document_root'] = '/srv/www/html'
-default['djshop_ubuntu']['app_root'] = '/srv/app/'
+default['djshop_ubuntu']['app_root'] = "/srv/app/#{app_name}"
+default['djshop_ubuntu']['git_repo'] = 'https://github.com/jccode/djshop.git'
+
--- chef/cookbooks/djshop_ubuntu/recipes/application.rb
+++ chef/cookbooks/djshop_ubuntu/recipes/application.rb
@@ -1,6 +1,23 @@
 
-# package 'git'
+package 'git'
 
 # application node['djshop_ubuntu']['app_root'] do
 #   git 'https://github.com/jccode/djshop.git'
 # end
+
+package 'python-dev'
+package 'python-virtualenv'
+
+git node['djshop_ubuntu']['app_root'] do
+  repository node['djshop_ubuntu']['git_repo']
+  revision 'master'
+  action :sync
+end
+
+application node['djshop_ubuntu']['app_root'] do
+  virtualenv
+  pip_requirements do
+    user "root"
+    group "root"
+  end
+end
