022eb3e6c85eec848c4b92ccbe4bcdd7e8ad6757
jessie <junchangchen@gmail.com>
2016-08-12 17:33:46 +0800

    mysql instance

--- chef/cookbooks/djshop_ubuntu/attributes/default.rb
+++ chef/cookbooks/djshop_ubuntu/attributes/default.rb
@@ -11,3 +11,7 @@ default['djshop_ubuntu']['document_root'] = '/srv/www/html'
 default['djshop_ubuntu']['app_root'] = "/srv/app/#{app_name}"
 default['djshop_ubuntu']['git_repo'] = 'https://github.com/jccode/djshop.git'
 
+default['djshop_ubuntu']['database']['dbname'] = 'djshop'
+default['djshop_ubuntu']['database']['host'] = '127.0.0.1'
+default['djshop_ubuntu']['database']['root_username'] = 'root'
+default['djshop_ubuntu']['database']['admin_username'] = 'djshop'
--- chef/cookbooks/djshop_ubuntu/recipes/database.rb
+++ chef/cookbooks/djshop_ubuntu/recipes/database.rb
@@ -6,36 +6,6 @@
 
 
 
-# puts "HELLO -------------- "+ENV['USER']
-
-
-# log 'message' do
-#   message 'Hello world'+ENV['USER']
-#   level :info
-# end
-
-# bash "echo something."+ENV['USERNAME']+":"+ENV['USER'] do
-#    code <<-EOF
-#      echo 'I am a chef!'+ENV['USERNAME']
-#    EOF
-# end
-
-
-# user 'mysql' do
-#   action :create
-# end
-
-
-# # Configure the MySQL client
-# mysql_client 'default' do
-#   action :create
-#   version '5.6'
-#   package_version ''
-#   run_user 'mysql'
-#   run_group 'mysql'
-# end
-
-
 mysql_config = data_bag_item("passwords", "mysql")
 
 # # Configure the MySQL service
@@ -48,13 +18,42 @@ mysql_config = data_bag_item("passwords", "mysql")
 #   run_group 'mysql'
 # end
 
+# Install mysql client
 mysql_client 'default' do
   action :create
   version '5.6'
 end
 
+# Install mysql server & configrate service
 mysql_service 'default' do
   version '5.6'
   initial_root_password mysql_config["password"]
   action [:create, :start]
+end
+
+# Install mysql2 Ruby gem
+mysql2_chef_gem 'default' do
+  action :install
+end
+
+# Create database instance
+mysql_database node['djshop_ubuntu']['database']['dbname'] do
+  connection(
+             :host => node['djshop_ubuntu']['database']['host'],
+             :username => node['djshop_ubuntu']['database']['root_username'],
+             :password => mysql_config["password"]
+            )
+  action :create
+end
+
+mysql_database_user node['djshop_ubuntu']['database']['admin_username'] do
+  connection(
+             :host => node['djshop_ubuntu']['database']['host'],
+             :username => node['djshop_ubuntu']['database']['root_username'],
+             :password => mysql_config["password"]
+            )
+  password node['djshop_ubuntu']['database']['password_admin']
+  database_name node['djshop_ubuntu']['database']['dbname']
+  host node['djshop_ubuntu']['database']['host']
+  action [:create, :grant]
 end
